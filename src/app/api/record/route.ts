c9fb38f23774aa1d9aa75cb9cac66bc5fa56c9b8
contraintuitiv <mail@contraintuitiv.de>
2024-08-21 23:42:39 +0200

    req body response

--- src/app/api/record/route.ts
+++ src/app/api/record/route.ts
@@ -7,5 +7,5 @@ export async function POST(req: NextRequest) {
     console.log(req.body)           
 
 
-    return NextResponse.json({ "req.body": "req.body" })
+    return NextResponse.json({ "req.body": req.body })
 }
