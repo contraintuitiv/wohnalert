e596d2faf2084390f33f33933ea4377d45b2c531
contraintuitiv <mail@contraintuitiv.de>
2024-08-21 23:59:39 +0200

    cut

--- src/app/api/record/route.ts
+++ src/app/api/record/route.ts
@@ -7,9 +7,12 @@ export async function GET() {
 }
 
 export async function POST(req: NextRequest) {
-    const recordBody = await req.text()      
-
-    console.log(recordBody)
+    const recordBody = (await req.text()).split("\n")
+    const url = recordBody[0]
+    const body = recordBody.slice(1)
+    
+    console.log("url",  url)
+    console.log("body",  body.join("\n"))
     
 
     return NextResponse.json({ "req.body": recordBody})
