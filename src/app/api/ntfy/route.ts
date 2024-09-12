16bde88baec7875db4842c2312ac18add0c5f408
Oscar <oscar@Oscars-iMac.fritz.box>
2024-09-12 14:59:46 +0200

    prettier doing prettier stuff

--- src/app/api/ntfy/route.ts
+++ src/app/api/ntfy/route.ts
@@ -1,19 +1,19 @@
-
-import { NextRequest, NextResponse } from "next/server";
-import { Ntfy, NtfyCreateInputSchema } from "../../../../prisma/generated/zod";
-import { ZodError } from "zod";
-import { prisma } from "../../../../lib/prisma";
-import { deconstructFilterQuery } from "../../../../lib/util";
+import { NextRequest, NextResponse } from 'next/server';
+import { Ntfy, NtfyCreateInputSchema } from '../../../../prisma/generated/zod';
+import { ZodError } from 'zod';
+import { prisma } from '../../../../lib/prisma';
+import { deconstructFilterQuery } from '../../../../lib/util';
 
 export async function GET(req: NextRequest) {
-    const { minRent, maxRent, minRooms, maxRooms, minSize, maxSize, boroughs } = deconstructFilterQuery(req.url)
+    const { minRent, maxRent, minRooms, maxRooms, minSize, maxSize, boroughs } =
+        deconstructFilterQuery(req.url);
 
     try {
         const existingNtfy = await prisma.ntfy.findFirst({
             select: {
                 id: true,
                 host: true,
-                topic: true
+                topic: true,
             },
             where: {
                 minRent,
@@ -22,38 +22,36 @@ export async function GET(req: NextRequest) {
                 maxRooms,
                 minSize,
                 maxSize,
-                boroughs: JSON.stringify(boroughs)
-            }
-        })
+                boroughs: JSON.stringify(boroughs),
+            },
+        });
 
-        return NextResponse.json(existingNtfy)
+        return NextResponse.json(existingNtfy);
     } catch {
-        return new NextResponse("Bad Request", { status: 403 })
+        return new NextResponse('Bad Request', { status: 403 });
     }
-
-
 }
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-    const body = await req.json() as Partial<Ntfy>
+    const body = (await req.json()) as Partial<Ntfy>;
 
     try {
         // validate
-        const data = NtfyCreateInputSchema.parse(body)
+        const data = NtfyCreateInputSchema.parse(body);
 
         const createdNtfy = await prisma.ntfy.create({
-            data: data
-        })
+            data: data,
+        });
 
-        return NextResponse.json(createdNtfy)
-
+        return NextResponse.json(createdNtfy);
     } catch (err) {
         if (err instanceof ZodError) {
-            return NextResponse.json({ error: err.errors }, { status: 400 })
+            return NextResponse.json({ error: err.errors }, { status: 400 });
         }
 
-        return NextResponse.json({ error: "db or unkonw error" }, { status: 400 })
-
+        return NextResponse.json(
+            { error: 'db or unknown error' },
+            { status: 400 }
+        );
     }
-
 }
