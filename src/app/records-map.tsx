045bbde221855105ded56b47adc7ef87797fb5e9
contraintuitiv <mail@contraintuitiv.de>
2024-09-23 18:59:20 +0200

    reparierte pipeline

--- src/app/records-map.tsx
+++ src/app/records-map.tsx
@@ -58,8 +58,8 @@ export default function RecordsMap({
     hoveredRecordId: number | null;
     centerCoords: [number, number] | null;
 }) {
-    // const { records } = useRecords();
-    const records = mockRecords;
+    const { records } = useRecords();
+    // const records = mockRecords;
 
     return (
         <MapContainer
@@ -99,7 +99,7 @@ export default function RecordsMap({
                             {record.road} {record.house_number}
                             <br />
                             <br />
-                            {/* {JSON.parse(record.properties).join(', ')} */}
+                            {JSON.parse(record.properties).join(', ')}
                         </Popup>
                     </Marker>
                 ))}
